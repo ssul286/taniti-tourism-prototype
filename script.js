c6f10f2bcd44804edb9130009e7dca8aa009b28a
ssul286 <ssul286@wgu.edu>
2026-02-04 18:39:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ function filterActivities(type) {
     }
   });
 }
+const form = document.getElementById('bookingForm');
+const modal = document.getElementById('confirmationModal');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form submission
+  modal.style.display = 'flex'; // Show the modal
+});
+
+function closeModal() {
+  modal.style.display = 'none'; // Hide the modal
+}
+
